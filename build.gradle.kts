e79df81a945278b660225c4feb2bc62c9d014430
Aswin Ashok-Arch <aswinashok3907@gmail.com>
2025-07-13 22:37:44 +0530

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+    id("java")
+    id("application")
+}
+
+group = "org.hospitalmanagement"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("mysql:mysql-connector-java:8.0.33")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "org.hospitalmanagement.Main"
+    }
+}
+application {
+    mainClass.set("org.hospitalmanagement.Main")
+}
+tasks.register<Jar>("fatJar") {
+    group = "build"
+    manifest {
+        attributes["Main-Class"] = "App" // or your full class name with package
+    }
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+    from(sourceSets.main.get().output)
+
+    dependsOn(configurations.runtimeClasspath)
+    from({
+        configurations.runtimeClasspath.get()
+            .filter { it.name.endsWith("jar") }
+            .map { zipTree(it) }
+    })
+
+    archiveClassifier.set("all") // output will be yourapp-all.jar
+}
---
+++ src/main/java/org/hospitalmanagement/Main.java
@@ -0,0 +1,12 @@
+package org.hospitalmanagement;
+import java.sql.*;
+
+class SQLconnection{
+
+}
+class Main{
+    public static void main(String[] args){
+        System.out.println("hellosi");
+    }
+}
+
